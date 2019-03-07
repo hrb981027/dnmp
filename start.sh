195fff70460c67be6012fc13a7ead4b3e0f8951a
hrb981027 <981027027@qq.com>
2019-03-07 11:47:25 +0800

    去除php-fpm慢日志，修改构建配置

--- start.sh
+++ start.sh
@@ -1,15 +1,10 @@
 #! /bin/bash
 
-#配置环境变量
-cp ./env.sample ./.env
-
-#修改文件夹所属者
+#修改数据及日志的权限
 chown -R 999:999 ./mariadb/data
 chown -R 999:999 ./mariadb/log
 chown -R 101:101 ./nginx/log
+chown -R 33:33   ./php-fpm/log
 
 #启动容器
 docker-compose up -d
-
-#修改php-fpm慢日志文件所属者
-[ -f "./php-fpm/log/slow.log" ] && chown 33:33 ./php-fpm/log/slow.log
